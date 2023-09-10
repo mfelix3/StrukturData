a1baf4fb218f3740075172f37718bb0cab4bbb5d
Michael Felix <michalefelix020@gmail.com>
2023-09-10 18:54:16 +0800

    git commit "update"

--- rearrange.py
+++ rearrange.py
@@ -1,9 +1,9 @@
 def rearrange(nums):
-    if len(nums) <= 10:
+    if len(nums) <= 5:
         return nums
     genap = [num for num in nums if num % 2 == 0]
     ganjil = [num for num in nums if num % 2 != 0]
 
     return rearrange(genap) + rearrange(ganjil)
 
-print (rearrange([3, 6, 8, 5, 4, 1, 4, 6]))
+print (rearrange([13, 17, 18, 15, 4, 22,35]))
