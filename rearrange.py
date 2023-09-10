7dc2501cd1c5235e8506af954008716f150eb157
Michael Felix <michalefelix020@gmail.com>
2023-09-10 18:40:02 +0800

    update

--- rearrange.py
+++ rearrange.py
@@ -1,8 +1,6 @@
 def rearrange(nums):
-    # base case
     if len(nums) <= 10:
         return nums
-    # recursive case
     even_nums = [num for num in nums if num % 2 == 0]
     odd_nums = [num for num in nums if num % 2 != 0]
 
